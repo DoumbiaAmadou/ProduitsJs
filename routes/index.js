e2cfb0f59b7ef6906fd474c181948e725ef86e6e
Doumbia Amadou <doumbia.amado@gmail.com>
2017-12-01 11:54:12 +0100

    produits

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,34 @@ var router = express.Router();
 
 /* unConnected  routes config */
 /* GET home page. */
+let objectToReturn = 
+        {
+            produits: [{ titre: "Pommes"},
+                { titre: "Bananes" , qte : 4 },
+                { titre: "Cerises", qte: 4 },
+                { titre: "Citron", qte: 4 },
+                { titre: "BPM", qte: 4},
+                { titre: "ALcools", qte: 4},
+                { titre: "Chaussures", qte: 4 },
+                { titre: "T-shirt", qte: 4 },
+                { titre: "Boxers", qte: 4},
+                { titre: "Slips", qte: 4},
+                { titre: "Sweats", qte: 4}
 
+            ]
+        };
 router.get('/', function (req, res) {
+    res.render('index', { title: 'Inventaire' });
+});
+router.get('/home', function (req, res) {
     res.render('index', { title: 'Inventaire' });
 });
 
 router.get('/articles', function (req, res) {
-    var objectToReturn = { produits: ["orange", "purple", "ad"] }; 
-    console.log(objectToReturn.produits);
-    res.render('index', objectToReturn);
+    
+
+    console.log(objectToReturn.produits); 
+    res.render('produits', objectToReturn);
 
 });
 
